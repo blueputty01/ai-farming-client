42c374d7efe3d297bf1232328a8490aedb68077f
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-11 14:49:08 -0400

    undoing cra

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, options) => {
+  const prod = options.mode === 'production';
+
+  const devOptions = prod ? null : { devtool: 'eval-source-map' };
+  return {
+    ...devOptions,
+    entry: './src/index.tsx',
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, './build'),
+      clean: true,
+      publicPath: '/',
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|jp2|webp)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+        chunkFilename: 'styles.css',
+      }),
+      // @ts-ignore
+      new ESLintPlugin({}),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        inject: true,
+      }),
+    ],
+    devServer: {
+      compress: true,
+      historyApiFallback: true,
+      open: true,
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+      },
+    },
+  };
+};
