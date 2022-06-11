ff0a0c07b3422cb451a0f53f1013814b186198e3
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-11 17:14:08 -0400

    set up api interfaces

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -2,13 +2,42 @@ import axios from 'axios';
 
 const url = 'localhost:5000';
 
+interface SearchRes {
+  imageURL: string,
+  itemHealth: string,
+  
+}
+
+interface PredictionResults {
+  imageURL: string,
+  itemHealth: string,
+  
+}
+
 class Server {
   getPrediction(file: File) {
-    return axios.post(`${url}/api/getPrediction`, file);
+    return axios.post(`${url}/getPrediction`, file);
   }
 
   postItem(file: File) {
-    return axios.post(`${url}/api/upload`, file);
+    return axios.post(`${url}/upload`, file);
+  }
+
+  search(query: string) {
+    return new Promise((resolve, reject) => {
+      try {
+        axios.get(url).then((res) => {
+          const data = res.data;
+          resolve(
+            data.map((marker):  => ({
+              ...marker,
+            }))
+          );
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
   }
 }
 
