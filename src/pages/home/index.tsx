42bb4b4677c02af127f4ca3d6f22a585133c1f65
Raghav Paratkar <raghavparatkar@gmail.com>
2022-06-11 17:15:39 -0400

    Changed the homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,15 @@
 import { useNavigate } from 'react-router-dom';
+import i from './shop.png';
 
 export default function Home() {
   return (
-    <main className="flex-1 wid w-9/10 text-center space-y-3">
-      <h1>Hi, welcome to</h1>
-      <h1 className="text-xl">LocalFoods</h1>
-      <nav className="flex space-x-4">
+    <main className="italic flex-1 wid w-9/10 text-center space-y-3 font-body text-gray-800 justify-center pt-5">
+      <h1 className="text-6xl font-black">Hi, welcome to</h1>
+      <div className="inline">
+        <img src={i} alt="..." height={600} width={600}></img>
+      </div>
+      {/* <h1 className="text-7xl">LocalFoods</h1> */}
+      <nav className="flex justify-center space-x-4">
         <HomeButton page={'post'} desc={'Add'}></HomeButton>
         <HomeButton page={'browse'} desc={'Browse'}></HomeButton>
         <HomeButton page={'diagnose'} desc={'Diagnose'}></HomeButton>
@@ -22,7 +26,7 @@ interface ButtonProps {
 function HomeButton(props: ButtonProps) {
   const navigate = useNavigate();
   const home =
-    'button bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded transition-colors duration-150';
+    'button-large bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-6 px-8 border border-blue-500 hover:border-transparent rounded ease-linear transition-all duration-150 text-5xl rounded-full';
   return (
     <button className={home} onClick={() => navigate(props.page)}>
       {props.desc}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      'display': ["'Oswald'"],
+      'body': ["'Open Sans'"],
+    },
+    fontSize: {
+       '5xl': '1.5rem',
+       '6xl': '3rem',
+       '7xl': '10rem',
+      },
     extend: {},
   },
   plugins: [],
