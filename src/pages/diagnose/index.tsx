fc9c1ee684b0c9200d30099efbbba92d85a09ebc
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-11 14:51:35 -0400

    full undo cra

---
+++ src/pages/diagnose/index.tsx
@@ -0,0 +1,10 @@
+import '../../index.css';
+
+export default function Diagnose() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">hello</h1>
+      <button type="button">Upload File</button>
+    </main>
+  );
+}
---
+++ src/pages/post/index.tsx
@@ -0,0 +1,7 @@
+export default function Post() {
+  return (
+    <div>
+      <h1>f</h1>
+    </div>
+  );
+}
