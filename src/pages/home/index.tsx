f2ff369d0e652378ba6cac4dc3aa4ce89ea7d55d
Raghav Paratkar <raghavparatkar@gmail.com>
2022-06-11 15:05:27 -0400

    Changed Buttons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,16 +7,16 @@ export default function Home() {
     return (
         <div>
             <div id="outer"><h1>
-                
+
                 Hi, welcome to
             </h1></div>
             <div id="outer"><h1>
                 LocalFoods
             </h1></div>
             <div id="outer">
-            <button type="button" className="inner" onClick={() => navigate('browse')}>Browse</button>
-            <button type="button" className="inner" onClick={() => navigate('post')}>Add Stuff</button>
-            <button type="button" className="inner" onClick={() => navigate('diagnose')}>Diagnose</button>
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => navigate('browse')}>Browse</button>
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => navigate('post')}>Add Stuff</button>
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => navigate('diagnose')}>Diagnose</button>
             </div>
 
     </div>
