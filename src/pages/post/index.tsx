4639e98ad89cad9ef7390ef07c3c27ccbe6c42fb
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-12 17:19:05 -0400

    remove redundant space

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -4,7 +4,7 @@ import Navigate from '../../shared/components/Navigation';
 export default function Post() {
   return (
     <main>
-      <Navigate title="Upload Plant's Leaf and Vegetable/Fruit"></Navigate>{' '}
+      <Navigate title="Upload Plant's Leaf and Vegetable/Fruit"></Navigate>
       <UploadButton className="p-1" mode={'predict'}></UploadButton>
       <UploadButton className="p-1" mode={'upload'}></UploadButton>
     </main>
