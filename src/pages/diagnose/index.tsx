f9dc42c4fadc169e1dc1a0010fd105e91ef42e06
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-12 19:36:58 -0400

    update desc

--- src/pages/diagnose/index.tsx
+++ src/pages/diagnose/index.tsx
@@ -17,8 +17,7 @@ export default function Diagnose() {
     <main className="flex-col text-center">
       <Navigate title="Diagnose"></Navigate>
       <h1 className="pb-10 5xl">
-        Upload an image of the fruit or its leaves in order to diagnose any
-        potential diseases
+        Upload an image of leaves in order to diagnose any potential diseases
       </h1>
       <UploadButton fileHandler={fileHandler}></UploadButton>
 
