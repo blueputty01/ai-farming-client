f95d2811a6e6410aa5bb46fe6da7e1b4575a506d
blue-putty <65861156+blue-putty@users.noreply.github.com>
2022-06-12 22:42:10 -0400

    clean up

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,6 +1,3 @@
-import UploadButton from '../../shared/components/Upload';
-import Navigate from '../../shared/components/Navigation';
-import { useState } from 'react';
 import Server from '../../shared/api';
 import Diagnosis from '../diagnose/Diagnosis';
 import Process from '../../shared/components/Process';
